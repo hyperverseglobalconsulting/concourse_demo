a2893afa404e23db62a4b61acf0746cd6ad55862
Vizeet Srivastava <vizeet.srivastava@datavid.com>
2023-08-13 21:32:54 +0200

    updated files

--- terraform/ebs.tf
+++ terraform/ebs.tf
@@ -1,7 +1,7 @@
 resource "aws_ebs_volume" "concourse_volumes" {
   count             = 3
   availability_zone = "us-east-2a" # Adjust this to your desired zone
-  size              = 5 # Size in GiB
+  size              = 20 # Size in GiB
   type              = "gp2"
 
   tags = {
