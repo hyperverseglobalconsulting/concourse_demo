42c93340bc5fb799d9157688c62472da3d00affa
Vizeet Srivastava <vizeet.srivastava@datavid.com>
2023-08-13 19:03:19 +0200

    added pv and pvc yaml files

---
+++ terraform/ebs.tf
@@ -0,0 +1,15 @@
+resource "aws_ebs_volume" "concourse_volumes" {
+  count             = 3
+  availability_zone = "us-east-2a" # Adjust this to your desired zone
+  size              = 5 # Size in GiB
+  type              = "gp2"
+
+  tags = {
+    Name = "concourse_volume-${count.index}"
+  }
+}
+
+output "ebs_volume_ids" {
+  value = aws_ebs_volume.concourse_volumes[*].id
+  description = "EBS Volume IDs for Concourse"
+}
