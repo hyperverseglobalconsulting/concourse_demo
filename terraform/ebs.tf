7f23f65bfad3038271a73f49e7cbaf73ccba0ef9
Vizeet Srivastava <vizeet.srivastava@datavid.com>
2023-08-16 00:26:54 +0200

    updated install to create pv

--- terraform/ebs.tf
+++ terraform/ebs.tf
@@ -2,7 +2,7 @@ resource "aws_ebs_volume" "concourse_volumes" {
   count             = 3
   availability_zone = "us-east-2a" # Adjust this to your desired zone
   size              = 20 # Size in GiB
-  type              = "gp2"
+  type              = "gp3"
 
   tags = {
     Name = "concourse_volume-${count.index}"
