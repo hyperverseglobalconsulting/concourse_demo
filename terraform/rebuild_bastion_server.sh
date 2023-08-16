0c82d5f5107eb89b9431bd80626147c6f5f202c7
Vizeet Srivastava <vizeet.srivastava@datavid.com>
2023-08-17 00:03:50 +0200

    rebuild bastion server to create port forwarding

--- terraform/rebuild_bastion_server.sh
+++ terraform/rebuild_bastion_server.sh
@@ -39,3 +39,5 @@ echo
 terraform plan -out=tfplan.out -var "sudo_password=$SUDO_PASSWORD"
 terraform apply "tfplan.out"
 
+BASTION_IP=$(terraform output -raw bastion_public_ip)
+ssh -i concourse-k8s.pem -L 8080:localhost:8080 ec2-user@$BASTION_IP -N &
